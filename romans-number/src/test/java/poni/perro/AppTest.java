c7d8ee69bb7cbb6b3dd890460b81b165657dd759
josepserra20 <jserravives@cifpfbmoll.eu>
2022-03-27 14:45:45 +0200

    estructure

---
+++ romans-number/src/test/java/poni/perro/AppTest.java
@@ -0,0 +1,12 @@
+package poni.perro;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
